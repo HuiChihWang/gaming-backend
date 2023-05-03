2ecf3bee5d763eadf8ae74e6181674bb8e5bda57
huichihwang <taya87136@gmail.com>
2023-05-03 17:47:20 +0800

    preload some characters in database

--- src/main/java/com/example/dbrelations/configuration/InitDatabase.java
+++ src/main/java/com/example/dbrelations/configuration/InitDatabase.java
@@ -1,8 +1,12 @@
 package com.example.dbrelations.configuration;
 
+import com.example.dbrelations.entity.GameCharacter;
 import com.example.dbrelations.entity.Player;
 import com.example.dbrelations.entity.Profile;
+import com.example.dbrelations.repository.GameCharacterRepository;
 import com.example.dbrelations.repository.PlayerRepository;
+import com.example.dbrelations.utility.Gender;
+import com.example.dbrelations.utility.Role;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
@@ -16,11 +20,28 @@ public class InitDatabase {
     private static final Logger logger =  LoggerFactory.getLogger(InitDatabase.class);
 
     @Bean
-    CommandLineRunner initDataBase(PlayerRepository repository) {
+    CommandLineRunner initDataBase(PlayerRepository playerRepository, GameCharacterRepository characterRepository) {
+        List<Player> players = createPlayers();
+        List<GameCharacter> characters = createCharacters();
+
+        List<Player> savedPlayers = playerRepository.saveAll(players);
+        List<GameCharacter> savedCharacters = characterRepository.saveAll(characters);
+        return args -> {
+            for (Player player: savedPlayers) {
+                logger.info("Preload Player: {}", player);
+            }
+
+            for (GameCharacter character: savedCharacters) {
+                logger.info("Preload Character: {}", character);
+            }
+        };
+    }
+
+    List<Player> createPlayers() {
         List<Profile> profiles = List.of(
-          new Profile("@gilbertwang", "gilbert_wang", "gilbertwang@gmail.com"),
-          new Profile("@albahuang", "alba_huang", "albahuang@gmail.com"),
-          new Profile("@kevinHu", "kevin_hu", "kevinhu@gmail.com")
+                new Profile("@gilbertwang", "gilbert_wang", "gilbertwang@gmail.com"),
+                new Profile("@albahuang", "alba_huang", "albahuang@gmail.com"),
+                new Profile("@kevinHu", "kevin_hu", "kevinhu@gmail.com")
         );
 
         List<Player> players = List.of(
@@ -33,12 +54,15 @@ public class InitDatabase {
             players.get(i).setProfile(profiles.get(i));
         }
 
-        List<Player> savedPlayers = repository.saveAll(players);
+        return players;
+    }
 
-        return args -> {
-            for (Player player: savedPlayers) {
-                logger.info("Preload Player: {}", player);
-            }
-        };
+    List<GameCharacter> createCharacters() {
+        return List.of(
+                new GameCharacter("taya87136", Gender.MALE, Role.WARRIOR),
+                new GameCharacter("sukilatic", Gender.FEMALE, Role.CLERIC),
+                new GameCharacter("gg5257", Gender.MALE, Role.MAGICIAN),
+                new GameCharacter("taya30621", Gender.FEMALE, Role.THIEF)
+        );
     }
 }
---
+++ src/main/java/com/example/dbrelations/repository/GameCharacterRepository.java
@@ -0,0 +1,7 @@
+package com.example.dbrelations.repository;
+
+import com.example.dbrelations.entity.GameCharacter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameCharacterRepository extends JpaRepository<GameCharacter, Long> {
+}
