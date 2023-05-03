b269f59aaabb24be3b80dfb8ed9bd6cda209d10d
huichihwang <taya87136@gmail.com>
2023-05-03 15:07:23 +0800

    create entity player and profile

---
+++ src/main/java/com/example/dbrelations/entity/Player.java
@@ -0,0 +1,90 @@
+package com.example.dbrelations.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "players")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private Profile profile;
+
+    public Player() {}
+
+    public Player(String firstName, String lastName, Profile profile) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profile = profile;
+    }
+
+    public Player(long id, String firstName, String lastName, Profile profile) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profile = profile;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return id == player.id && Objects.equals(firstName, player.firstName) && Objects.equals(lastName, player.lastName) && Objects.equals(profile, player.profile);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profile_id='" + profile.getId() + '\'' +
+                ", profile_email='" + profile.getEmail() + '\'' +
+                '}';
+    }
+}
