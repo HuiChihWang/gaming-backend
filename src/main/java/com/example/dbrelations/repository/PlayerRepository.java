cdf28252908df04148f4813d295cc808ecdc496b
huichihwang <taya87136@gmail.com>
2023-05-03 15:07:40 +0800

    create repository for player and profile

---
+++ src/main/java/com/example/dbrelations/repository/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.example.dbrelations.repository;
+
+import com.example.dbrelations.entity.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
