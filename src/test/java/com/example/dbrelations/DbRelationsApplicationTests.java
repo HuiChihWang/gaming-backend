668f6238b24491e120f284f7e8a0a4ef95ac9100
huichihwang <taya87136@gmail.com>
2023-05-03 14:23:25 +0800

    initialize project

---
+++ src/test/java/com/example/dbrelations/DbRelationsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.dbrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assert(true);
+    }
+
+}
