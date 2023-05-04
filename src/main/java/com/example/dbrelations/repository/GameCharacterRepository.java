fdf85b4391ef9860e9fac3b125960011c21c3bf5
huichihwang <taya87136@gmail.com>
2023-05-04 11:28:35 +0800

    add new filter method in GameCharacterRepository

--- src/main/java/com/example/dbrelations/repository/GameCharacterRepository.java
+++ src/main/java/com/example/dbrelations/repository/GameCharacterRepository.java
@@ -4,4 +4,5 @@ import com.example.dbrelations.entity.GameCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameCharacterRepository extends JpaRepository<GameCharacter, Long> {
+    boolean existsByName(String name);
 }
